ebb9916899f2df7e4331464bc0328b9a033ad0f6
HaiYangCode <kk@gmail.com>
2023-03-22 16:02:18 +0800

    dev:adapter实现数据更新

--- databindingdemo/src/main/java/com/example/databindingdemo/MainViewModel.kt
+++ databindingdemo/src/main/java/com/example/databindingdemo/MainViewModel.kt
@@ -9,15 +9,29 @@ class MainViewModel : ViewModel() {
         "https://www.travelrich.com.tw/data/766716b8-48ea-4f5c-a080-cf9c6024d50a.jpg"
     var defaultImage = ""
     var adapter: RvAdapter
+    val data = mutableListOf<User>()
 
     init {
-        val data = listOf<User>(
-            User("zhagnsan", 23),
-            User("lisi", 22),
-            User("wagnwu", 21),
-            User("shenliu", 20)
-        )
+        data.add(User("zhagnsan", 23))
+        data.add(User("lisi", 22))
+        data.add(User("wagnwu", 21))
+        data.add(User("shenliu", 20))
         adapter = RvAdapter(data)
     }
+
+    fun updateData() {
+        data.addAll(randomData())
+        adapter.notifyDataSetChanged()
+    }
+
+    fun randomData(): MutableList<User> {
+        val data = mutableListOf<User>()
+        data.add(User("zhagnsan", 23))
+        data.add(User("lisi", 22))
+        data.add(User("wagnwu", 21))
+        data.add(User("shenliu", 20))
+        return data
+    }
+
 
 }
